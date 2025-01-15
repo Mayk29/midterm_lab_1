d71c55be8354fa89ea2d9e00e60b3c4e3715f207
System Administrator <root@SJH-706-38.local>
2025-01-15 10:33:43 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CommonModule, DatePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DatePipe,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pipes-demo';
+  presentDate = new Date();
+  time$ = interval(1000).pipe(map(()=> new Date()));
+  testObject ={
+    name: 'Michael Ronquillo',
+    age:21,
+    food:'Sisig',
+    course: 'BSIT Web Development'
+  };
+  testArray =[1,2,3,4,5];
+  price: number = 20000;
+  Fruits = ['Apple','Orange','Grapes','Mango','Kiwi','Pomegranate'];
+  decimalNum1: number = 8.7589623;
+  decimalNum2: number = 5.43;
+
 }
